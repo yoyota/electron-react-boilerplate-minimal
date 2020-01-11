720e881513f5583737d733269a5b772cdac8c62e
yoyota <smarthorse6@gmail.com>
2020-01-11 13:45:45 +0900

    Cut off from upstream https://github.com/electron-react-boilerplate/electron-react-boilerplate

--- app/index.js
+++ app/index.js
@@ -1,17 +1,14 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
-import './app.global.css';
+import App from "./App"
 
-const store = configureStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
 render(
   <AppContainer>
-    <Root store={store} history={history} />
+    <App />
   </AppContainer>,
   document.getElementById('root')
 );
